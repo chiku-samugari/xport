f2e6dd0859c3d0fe46324e39f5a5d7b692f5b46c
chiku <samugari@is.s.u-tokyo.ac.jp>
2020-06-21 01:52:05 +0900

    XDEFMACRO that defines macro and exports the symbol names the macro.

--- src/xport.lisp
+++ src/xport.lisp
@@ -1,11 +1,18 @@
 (in-package :xport)
 
-(export
-  (defmacro xport-when (situations &body forms)
-    `(eval-when ,situations
-       (export
-         ,@forms)))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (export
+    (defmacro xport-when (situations &body forms)
+      `(eval-when ,situations
+         (export
+           ,@forms))))
 
-  (defmacro xport (&body forms)
-    `(xport-when (:compile-toplevel :load-toplevel :execute)
-       ,@forms)))
+  (export
+    (defmacro xport (&body forms)
+      `(xport-when (:compile-toplevel :load-toplevel :execute)
+         ,@forms))))
+
+(export
+  (defmacro xdefmacro (name lambda-list &body body)
+    `(xport
+       (defmacro ,name ,lambda-list ,@body))))
